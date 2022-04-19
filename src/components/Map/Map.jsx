e86347e65d01a56e34e364b11fe18685ebe02c72
QQRyan <ryanliu383@gmail.com>
2022-04-18 21:04:27 -0400

    add ratings

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-import { LocationOnOutlinedIcon } from '@material-ui/icons/LocationOnOutlined';
-import Rating from '@material-ui/lab';
+import  LocationOnOutlinedIcon  from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
     const classes = useStyles();
-    const isMobile = useMediaQuery('(min-width:600px)');
+    const isDesktop = useMediaQuery('(min-width:600px)');
 
     const coords = { lat: 0, lng: 0 };
 
@@ -27,7 +27,32 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
                 }}
                 onChildclick={''}
             >
-
+                {places?.map((place, i) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                    >
+                        {
+                            !isDesktop ? (
+                                <LocationOnOutlinedIcon color="primary" fontSize="large"/>
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="subtitle2" gutterBottom>
+                                        {place.name}
+                                    </Typography>
+                                    <img
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://i.etsystatic.com/24305988/r/il/274a22/2462601398/il_1588xN.2462601398_lsge.jpg'}
+                                        alt={place.name}
+                                    />
+                                    <Rating size="small" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
             </GoogleMapReact>
         </div>
     );
